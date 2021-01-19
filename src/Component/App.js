47ea12a689b6e042458c23cdd6a72b28d2a53a1a
Gwenael Even <gwenael.even.pro@gmail.com>
2021-01-19 22:34:40 +0100

    about css

--- src/Component/App.js
+++ src/Component/App.js
@@ -4,13 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row } from 'react-bootstrap'
 import Hamburger from './Nav/Hamburger'
 import Landpage from './Landpage/Landpage'
+import About from './About/About'
+
 
 function App() {
   return (
     <>
       <Container fluid='sm' className={'noPadding noMargin'}>
-              <Landpage />
-              <Hamburger />
+        <div className={'maxSize centerComponent'}>
+          <Landpage />
+        </div>
+        <div className={'maxSize centerComponent'}>
+          <About />
+        </div>
+        <Hamburger />
       </Container>
     </>
   )
