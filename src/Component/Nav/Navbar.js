50557817fdfb63ca7f30b119cd45ea1e00871a27
Gwenael Even <gwenael.even.pro@gmail.com>
2021-01-19 22:52:58 +0100

    fixed navbar + overflow

--- src/Component/Nav/Navbar.js
+++ src/Component/Nav/Navbar.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Col } from 'react-bootstrap'
 
 function Navbar (props) {
 
+    useEffect(() => {
+        if(props.active) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'visible'
+
+        }
+    })
+
     return (
             <div className={`navBarMenu ${props.active ? 'navBarActive' : ''}`}>
-                <div className={'navBarLink'}>
+                <nav className={'navBarLink'}>
                     <ul>
                         <li className={'liNavbar'}><a href='#'>Accueil</a></li>
                         <li className={'liNavbar'}><a href='#'>A propos</a></li>
@@ -13,7 +22,7 @@ function Navbar (props) {
                         <li className={'liNavbar'}><a href='#'>Mes projets</a></li>
                         <li className={'liNavbar'}><a href='#'>Me contacter</a></li>
                     </ul>
-                </div>
+                </nav>
             </div>
     )
 }
